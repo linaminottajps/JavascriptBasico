cddda69d49f2fefd08c7f6f96c26f5b3c1b23240
Lina Minotta Medina <lina.minotta@flyr.com>
2026-03-06 06:41:20 -0500

    Practica de Variables

--- js/variables.js
+++ js/variables.js
@@ -1,2 +1,35 @@
-console.log("Variables in JavaScript");
+console.log("Variables JavaScript Lina");
+
+// ¿ Que es una variable? 
+
+// [] string // nunber // bool
+
+// -- Let -const
+let age = 18;
+console.log("age", age)
+
+const firtName = "juliana";
+console.log("mi nombre es :", firtName);
+
+//firtName = "juliana perez";
+//console.log("mi nombre actualizado es: ", firstName);
+
+//convenciones
+//nombre de las variables en camelCase
+//nombre descriptivo
+
+const bundles = "countries"
+
+
+//Declarar variables que describan a una persona e imprimir en consola
+
+const nombre = "Lina Minotta"
+let edad = 37;
+let peso = 61
+let estatura = 1.61;
+const genero = "femenino"
+const RH = "O+"
+
+console.log("esta es mi descripcion:","Soy", nombre, "mi edad es:", edad, "años,", "mi peso es:", peso, "Kg,", "mi estatura es:", estatura,"cm,", "mi genero es:", genero, "mi RH es:", RH)
+
 
